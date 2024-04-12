79964adb21140bbf626a385e46c91b55b8e74e5c
karthi22308 <47984454+karthi22308@users.noreply.github.com>
2024-04-12 23:07:07 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
 
+
+#include <iostream>
+using namespace std;
+
+class Pet{
+  int PetId;
+  int petage;
+  
+};
+class user{
+    
+    
+};
+
+class praani{
+    
+    public:
+    vec
+    
+    
+};
+
+
+int main()
+{
+
+cout<<"Hello World";
+
+return 0;
+}
+

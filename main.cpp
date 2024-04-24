2789a16c853fb143ced3a484808730331a052495
karthi22308 <47984454+karthi22308@users.noreply.github.com>
2024-04-24 23:19:25 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,54 @@
-
-
 #include <iostream>
+#include<string>
+#include<vector>
+#include<unordered_map>
 using namespace std;
 
 class Pet{
   int PetId;
-  int petage;
+string pettype;
+  int pet_age;
+  bool isadopted;
+  string pincode;
+  string piclink;
+  int userid;
+  int Auserid;
+  bool custody;
+string userdescription
+  
   
 };
-class user{ ...
+class user{ 
+    int userid;
+    string name;
+    string number;
+string password;
      
     
 };
 
 class praani{
+public:
+vector<Pet> PetList;
+vector<user> userlist;
+vector<string> pettype;
+int petid
+praani(){
+    petid=1;
     
-    public:
-    vec
+    pettype.push_back("cat");
+     pettype.push_back("dog");
+    
+}
+void addpet(){
+    Pet p;
+    p.petid=petid;
+    cout<<"select pet "
+    
+}
+
+    
+
     
     
 };
@@ -24,9 +56,25 @@ class praani{
 
 int main()
 {
-
-cout<<"Hello World";
+    bool runner= true;
+praani obj;    
+while(runner){
+cout<<"welcome to Praani application"<<endl;
+cout<<"#Adopt.#Don't shop"<<endl;
+cout<<"please slect an option to proceed"<<end;
+cout<<"1.Report and save a pet"<<endl;
+    cout<<"2.Adopt and change a life of a pet"<<endl;
+    cout<<"3.close application"<<endl;
+int n;
+    cin>>n;
+    switch(n){
+        case 1:
+        case 2:
+        case 3:runner=false;break;
+    }
+    
+    
+}
 
 return 0;
 }
-
